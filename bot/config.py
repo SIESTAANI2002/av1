a717781d629d0c2280979f803944efcbd4705e94
SIESTAANI2002 <107175442+SIESTAANI2002@users.noreply.github.com>
2023-04-26 10:06:28 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -18,12 +18,12 @@ from decouple import config
 try:
     APP_ID = config("APP_ID", default=15681388, cast=int)
     API_HASH = config("API_HASH", default="446b56944f74f6b7688175d48cdfa881")
-    BOT_TOKEN = "6249960795:AAHBC233MxzMDnrtTiBP8FcmfltympbBpiE"
+    BOT_TOKEN = "6130252508:AAF3L43Ry6ttkEQKtLAzQ9u23kPvefLbHWk"
     DEV = 5074446156
     OWNER = config("OWNER", default="5074446156")
     FFMPEG = config(
         "FFMPEG",
-        default='ffmpeg -i "{}" -map 0:v? -map 0:a? -map 0:s? -c:v libsvtav1 -pix_fmt yuv420p10le -preset 6 -color_primaries 1 -color_range 1 -color_trc 1 -colorspace 1 -s 1280:720 -svtav1-params "tune=1:pass=0:film-grain=4:keyint=240:profile=0:lp=4:enable-hdr=1"  -crf 30 -c:a aac -b:a 96k -c:s copy -metadata title="Presented By Ani Animesh" -metadata author="AnimeSakura.co" -metadata copyright="Copyright 2023 Me" -metadata encoded_by="Lolikiller #Sakura_Gang" -metadata:s:0 title="Encoded By Ani Animesh" -metadata:s:a:0 title="Ani Animesh" -metadata:s:a:1 title="AnimeSakura.co" -metadata:s:s:0 title="AnimeSakura.co" -metadata:s:s:1 title="@Ani_Animesh" "{}"',
+        default='ffmpeg -i "{}" -map 0:v? -map 0:a? -map 0:s? -c:v libsvtav1 -pix_fmt yuv420p10le -preset 6 -s 1280:720 -svtav1-params "tune=1:pass=0:film-grain=4:keyint=240:profile=0:lp=4:enable-hdr=1"  -crf 30 -c:a aac -b:a 96k -c:s copy -metadata title="Presented By Ani Animesh" -metadata author="AnimeSakura.co" -metadata copyright="Copyright 2023 Me" -metadata encoded_by="Lolikiller #Sakura_Gang" -metadata:s:0 title="Encoded By Ani Animesh" -metadata:s:a:0 title="Ani Animesh" -metadata:s:a:1 title="AnimeSakura.co" -metadata:s:s:0 title="AnimeSakura.co" -metadata:s:s:1 title="@Ani_Animesh" "{}"',
     )
     THUMB = config(
         "THUMBNAIL", default="https://graph.org/file/a615291ef3f9f361e9b12.jpg"
